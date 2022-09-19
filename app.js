267ae36f6a1fb8fbf46949e8b8d8053ef7435637
pavithra <pavithraravi1902@gmail.com>
2022-09-19 23:42:18 +0530

    update

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ var con = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "root",
-    database: "quiz_data"
+    database: "student_data"
 });
 
 //check database connection
@@ -28,14 +28,15 @@ con.connect(function (err) {
     console.log("Connected");
 });
 
-//get all data
+//get all student data
 
-app.get('/user', (req, res) => {
-    var sql = "select * from quiz_ans"
+app.get('/student', (req, res) => {
+    var sql = `select 
+    id, reg_no, username, age, dept
+    from student_info`;
     con.query(sql, function (err, result) {
         if (err) {
             throw err;
-            console.log(result);
         }
         if (result.length > 0) {
             res.send({
@@ -46,10 +47,10 @@ app.get('/user', (req, res) => {
     });
 });
 
-//get single data
-app.get('/user/:id', (req, res) => {
+//get single student data
+app.get('/student/:id', (req, res) => {
     let gID = req.params.id;
-    var sql = `select * from quiz_ans where id= ${gID}`;
+    var sql = `select * from student_info where id= ${gID}`;
     con.query(sql, function (err, result) {
         if (err) {
             throw err;
@@ -68,29 +69,20 @@ app.get('/user/:id', (req, res) => {
     });
 });
 
-//create data
-app.post('/user', (req, res) => {
+//create student data
+app.post('/student', (req, res) => {
     console.log("create data");
     let ID = req.body.id;
-    let user_name = req.body.User_name;
-    let ans1 = req.body.Ans1;
-    let ans2 = req.body.Ans2;
-    let ans3 = req.body.Ans3;
-    let ans4 = req.body.Ans4;
-    let ans5 = req.body.Ans5;
-    let ans6 = req.body.Ans6;
-    let ans7 = req.body.Ans7;
-    let ans8 = req.body.Ans8;
-    let ans9 = req.body.Ans9;
-    let ans10 = req.body.Ans10;
-    let marks = req.body.Marks;
+    let Reg_No = req.body.reg_no;
+    let user_name = req.body.username;
+    let Age = req.body.age;
+    let Dept = req.body.dept;
 
-    let sql = `insert into quiz_ans(id, User_name, Ans1, Ans2, Ans3, Ans4, Ans5, Ans6, Ans7, Ans8, Ans9, Ans10, Marks)
-               values ('${ID}', '${user_name}', '${ans1}', '${ans2}', '${ans3}', '${ans4}', '${ans5}', '${ans6}', '${ans7}', '${ans8}', '${ans9}', '${ans10}', '${marks}')`;
+    let sql = `insert into student_info(id, reg_no, username, age, dept)
+               values ('${ID}', '${Reg_No}', '${user_name}', '${Age}', '${Dept}')`;
     con.query(sql, function (err, result) {
         if (err) {
             throw err;
-            console.log(err);
         }
         console.log("result", result);
         res.send({
@@ -101,34 +93,42 @@ app.post('/user', (req, res) => {
 });
 
 
-//update single data
-app.put('/user/:id', (req, res) => {
+//update single student data
+app.put('/student/:id', (req, res) => {
     console.log("update data");
 
     let gID = req.params.id;
     let ID = req.body.id;
-    let user_name = req.body.User_name;
-    let ans1 = req.body.Ans1;
-    let ans2 = req.body.Ans2;
-    let ans3 = req.body.Ans3;
-    let ans4 = req.body.Ans4;
-    let ans5 = req.body.Ans5;
-    let ans6 = req.body.Ans6;
-    let ans7 = req.body.Ans7;
-    let ans8 = req.body.Ans8;
-    let ans9 = req.body.Ans9;
-    let ans10 = req.body.Ans10;
-    let marks = req.body.Marks;
+    let Reg_No = req.body.reg_no;
+    let user_name = req.body.username;
+    let Age = req.body.age;
+    let Dept = req.body.dept;
 
-    let sql = `update quiz_ans set User_name = ${'user_name'} where id= ${gID}`;
+
+    let sql = `update student_info set age = '${Age}' where id= '${gID}'`;
+    let sql1 = `update student_info set username = '${user_name}' where id= '${gID}'`;
+    let sql2 = `update student_info set reg_no = '${Reg_No}' where id= '${gID}'`;
+    let sql3 = `update student_info set dept = '${Dept}' where id= '${gID}'`;
+    console.log('UPDATE QUERY: ', sql);
     con.query(sql, (err, result) => {
         if (err) { console.log(err); }
         res.send({
             message: "data updated"
         });
-    })
+    });
 });
 
+//delete single data
+app.delete('/student/:id', (req, res) => {
+    let qID = req.params.id;
+    let sql = `delete from student_info where id='${qID}'`;
+    con.query(sql, (err, result) => {
+        if (err) { console.log(err); }
+        res.send({
+            message: "data deleted"
+        });
+    });
+});
 
 app.listen(3000, () => {
     console.log("Running PORT 3000")
